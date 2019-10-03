6b86f227490e58fa3f909d53981202ddb42cb33d
Mário Barbosa <mariogomes@MacBook-Pro-de-Mario.local>
2019-10-03 12:26:40 -0300

    ajustando thumbnail no spot.js

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const SpotSchema = new mongoose.Schema({
-    thubnail: String,
+    thumbnail: String,
     company: String,
     price: Number,
     techs: [String],
