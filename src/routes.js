acc413beed8c1645582aa96ca0c31aca50d13bd1
Mário Barbosa <mariogomes@MacBook-Pro-de-Mario.local>
2019-10-03 13:53:53 -0300

    finishing backend

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const multer = require('multer')
 const uploadConfig = require('./config/upload')
+
+// Controllers Imports
 const SessionController = require('./controllers/SessionController')
 const SpotController = require('./controllers/SpotController')
+const DashboardController = require('./controllers/DashboardController')
+const BookingController = require('./controllers/BookingController')
 
 const routes = express.Router()
 const upload = multer(uploadConfig)
@@ -10,5 +14,12 @@ const upload = multer(uploadConfig)
 routes.post('/sessions', SessionController.store)
 
 routes.post('/spots', upload.single('thumbnail'), SpotController.store)
+routes.get('/spots', SpotController.index)
+
+routes.get('/dashboard', DashboardController.show)
+
+routes.post('/spots/:spot_id/bookings', BookingController.store)
+
+
 
 module.exports = routes
