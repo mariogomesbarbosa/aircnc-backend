706f6a393fdc34a6aef3ab7cceeb7ae5c93b7ee3
Mário Barbosa <mariogomes@MacBook-Pro-de-Mario.local>
2019-10-02 23:23:45 -0300

    tentando fazer funcionar a rota spot mas falhando mizeravelmente

--- src/server.js
+++ src/server.js
@@ -1,3 +1,9 @@
+// GET = Listar, POST = Criar, PUT = Editar, DELETE = Deletar
+
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
+
 const express = require('express')
 const routes = require('./routes')
 const mongoose = require('mongoose')
@@ -8,12 +14,6 @@ mongoose.connect('mongodb+srv://mariobarbosa:049632531@cluster0-moi0f.mongodb.ne
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-
-// GET = Listar, POST = Criar, PUT = Editar, DELETE = Deletar
-
-// req.query = Acessar query params (para filtros)
-// req.params = Acessar route params (para edição, delete)
-// req.body = Acessar corpo da requisição (para criação, edição)
 
 app.use(express.json())
 app.use(routes)
