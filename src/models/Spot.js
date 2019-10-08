83185d0f85d54e0265e88012a561a359aea8ee52
Mário Barbosa <mariogomes@MacBook-Pro-de-Mario.local>
2019-10-07 21:58:52 -0300

    ajustando path para heroku em spot thumbnail virtual route

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -16,7 +16,7 @@ const SpotSchema = new mongoose.Schema({
 })
 
 SpotSchema.virtual('thumbnail_url').get(function() {
-    return `http://localhost:3000/files/${this.thumbnail}`
+    return `https://aircnc-back-end.herokuapp.com/files/${this.thumbnail}`
 })
 
 module.exports = mongoose.model('Spot', SpotSchema)
