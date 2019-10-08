dd00cc8772f6e1031f0fab53cef9bff43ec0182b
Mário Barbosa <mariogomes@MacBook-Pro-de-Mario.local>
2019-10-07 21:34:53 -0300

    removing cors

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@
 // req.body = Acessar corpo da requisição (para criação, edição)
 
 const mongoose = require('mongoose')
-const cors = require('cors')
+// const cors = require('cors')
 const path = require('path')
 const express = require('express')
 const routes = require('./routes')
@@ -17,7 +17,7 @@ mongoose.connect('mongodb+srv://mariobarbosa:049632531@cluster0-moi0f.mongodb.ne
     useUnifiedTopology: true,
 })
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes)
