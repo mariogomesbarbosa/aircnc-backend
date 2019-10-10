04a5b7d0609017ef864481a3f4c7cda279a5ba6d
Mário Barbosa <mariogomes@MacBook-Pro-de-Mario.local>
2019-10-10 14:09:07 -0300

    Feature: configuring socket.io

--- src/server.js
+++ src/server.js
@@ -8,14 +8,36 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const path = require('path')
 const express = require('express')
+
+const socketio = require('socket.io')
+const http = require('http')
+
 const routes = require('./routes')
 
 const app = express()
 const PORT = process.env.PORT || 3000;
+const server = http.Server(app)
+const io = socketio(server)
 
 mongoose.connect('mongodb+srv://mariobarbosa:049632531@cluster0-moi0f.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+})
+
+const connectedUsers = {}
+
+io.on('connection' , socket => {
+
+    const { user_id } = socket.handshake.query
+    
+    connectedUsers[user_id] = socket.id
+})
+
+app.use((req, res, next) => {
+    req.io = io
+    req.connectedUsers = connectedUsers
+
+    return next()
 })
 
 app.use(cors())
@@ -23,4 +45,4 @@ app.use(express.json())
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes)
 
-app.listen(PORT)
+server.listen(PORT)
